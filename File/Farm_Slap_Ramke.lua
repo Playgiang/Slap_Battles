dab65aa019d1c6624311ccefd771857e11a03ab2
Playgiang <gn9392567@gmail.com>
2024-08-27 01:19:20 +0700

    Update Farm_Slap_Ramke.lua

--- File/Farm_Slap_Ramke.lua
+++ File/Farm_Slap_Ramke.lua
@@ -1,1 +1,200 @@
-test
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if game.PlaceId == 6403373529 or game.PlaceId == 9015014224 then
+if not _G.Setting then
+_G.Setting = {
+   Teleport = "SafeSpotBox 1.0",
+   ChooseWillEnd = "Reset",
+   ResetLoop = true,
+   Slap = 50,
+   Time = 5
+}
+end
+
+if workspace:FindFirstChild("SafeBox") == nil then
+local S = Instance.new("Part")
+S.Name = "SafeBox"
+S.Anchored = true
+S.CanCollide = true
+S.Transparency = .5
+S.Position = Vector3.new(-5500, -5000, -5000)
+S.Size = Vector3.new(21, 5, 21)
+S.Parent = workspace
+
+local S1 = Instance.new("Part")
+S1.Name = "S1"
+S1.Anchored = true
+S1.CanCollide = true
+S1.Transparency = .5
+S1.Position = Vector3.new(-5499.91, -4991.5, -4989.09)
+S1.Size = Vector3.new(20, 13, 2)
+S1.Parent = workspace:FindFirstChild("SafeBox")
+
+local S2 = Instance.new("Part")
+S2.Name = "S2"
+S2.Anchored = true
+S2.CanCollide = true
+S2.Transparency = .5
+S2.Position = Vector3.new(-5510.27979, -4991.5, -5000.08984, -4.47034836e-07, 0, 0.999999881, 0, 1, 0, -0.999999881, 0, -3.69319451e-07)
+S2.Size = Vector3.new(21, 14, 2)
+S2.Rotation = Vector3.new(0, -90, 0)
+S2.Parent = workspace:FindFirstChild("SafeBox")
+
+local S3 = Instance.new("Part")
+S3.Name = "S3"
+S3.Anchored = true
+S3.CanCollide = true
+S3.Transparency = .5
+S3.Position = Vector3.new(-5499.3, -4991.5, -5011.12)
+S3.Size = Vector3.new(21, 13, 2)
+S3.Parent = workspace:FindFirstChild("SafeBox")
+
+local S4 = Instance.new("Part")
+S4.Name = "S4"
+S4.Anchored = true
+S4.CanCollide = true
+S4.Transparency = .5
+S4.Position = Vector3.new(-5489.97559, -4991.5, -4999.52637, -4.37113883e-08, 0, 1, 0, 1, 0, -1, 0, -4.37113883e-08)
+S4.Size = Vector3.new(22, 13, 2)
+S4.Rotation = Vector3.new(0, -90, 0)
+S4.Parent = workspace:FindFirstChild("SafeBox")
+
+local S5 = Instance.new("Part")
+S5.Name = "S5"
+S5.Anchored = true
+S5.CanCollide = true
+S5.Transparency = .5
+S5.Position = Vector3.new(-5499.39, -4984, -5000.07)
+S5.Size = Vector3.new(24, 3, 24)
+S5.Parent = workspace:FindFirstChild("SafeBox")
+end
+
+---SafeSpot---
+
+if workspace:FindFirstChild("Safespot") == nil then
+local Safespot = Instance.new("Part",workspace)
+Safespot.Name = "Safespot"
+Safespot.Position = Vector3.new(10000,-50,10000)
+Safespot.Size = Vector3.new(500,10,500)
+Safespot.Anchored = true
+Safespot.CanCollide = true
+Safespot.Transparency = .5
+
+local Safespot1 = Instance.new("Part",workspace)
+Safespot1.Name = "DefendPart"
+Safespot1.Position = Vector3.new(10000.2, 13, 9752.45)
+Safespot1.Size = Vector3.new(500, 117, 5)
+Safespot1.Anchored = true
+Safespot1.CanCollide = true
+Safespot1.Transparency = .5
+Safespot1.Parent = game.workspace.Safespot
+
+local Safespot2 = Instance.new("Part",workspace)
+Safespot2.Name = "DefendPart1"
+Safespot2.Position = Vector3.new(10248.2, 13, 10002.4)
+Safespot2.Size = Vector3.new(5, 117, 496)
+Safespot2.Anchored = true
+Safespot2.CanCollide = true
+Safespot2.Transparency = .5
+Safespot2.Parent = game.workspace.Safespot
+
+local Safespot3 = Instance.new("Part",workspace)
+Safespot3.Name = "DefendPart2"
+Safespot3.Position = Vector3.new(9998.13, 13, 10247.2)
+Safespot3.Size = Vector3.new(497, 117, 6)
+Safespot3.Anchored = true
+Safespot3.CanCollide = true
+Safespot3.Transparency = .5
+Safespot3.Parent = game.workspace.Safespot
+
+local Safespot4 = Instance.new("Part",workspace)
+Safespot4.Name = "DefendPart3"
+Safespot4.Position = Vector3.new(9752.71, 13, 9999.28)
+Safespot4.Size = Vector3.new(7, 117, 490)
+Safespot4.Anchored = true
+Safespot4.CanCollide = true
+Safespot4.Transparency = .5
+Safespot4.Parent = game.workspace.Safespot
+
+local Safespot5 = Instance.new("Part",workspace)
+Safespot5.Name = "DefendPart4"
+Safespot5.Position = Vector3.new(10001.1, 76, 9999.66)
+Safespot5.Size = Vector3.new(491, 10, 491)
+Safespot5.Anchored = true
+Safespot5.CanCollide = true
+Safespot5.Transparency = .5
+Safespot5.Parent = game.workspace.Safespot
+end
+
+if _G.Setting.Teleport == "Up To You" then
+OGL = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+end
+if game.Players.LocalPlayer.Character:FindFirstChild("entered") == nil and game.Players.LocalPlayer.leaderstats.Glove.Value == "Reverse" then
+game:GetService("ReplicatedStorage"):WaitForChild("ReverseAbility"):FireServer()
+wait()
+fireclickdetector(workspace.Lobby.Replica.ClickDetector)
+wait(0.01)
+repeat task.wait() until game.Players.LocalPlayer.Character
+if not game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+repeat task.wait()
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), workspace.Lobby.Teleport2.TouchInterest.Parent, 0)
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), workspace.Lobby.Teleport2.TouchInterest.Parent, 1)
+until game.Players.LocalPlayer.Character:FindFirstChild("entered")
+end
+wait(0.04)
+if _G.Setting.Teleport == "Up To You" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = OGL
+elseif _G.Setting.Teleport == "SafeSpotBox 1.0" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace["SafeBox"].CFrame * CFrame.new(0,5,0)
+elseif _G.Setting.Teleport == "SafeSpotBox 2.0" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace["Safespot"].CFrame * CFrame.new(0,10,0)
+end
+wait(0.23)
+game:GetService("ReplicatedStorage").Duplicate:FireServer()
+wait(0.2)
+for i = 1, _G.Setting.Time do
+if game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character.Humanoid.Health ~= 0 then
+for i, v in pairs(workspace:GetChildren()) do 
+                 if v.Name:match(game.Players.LocalPlayer.Name) and v:FindFirstChild("HumanoidRootPart") then
+for i = 1, _G.Setting.Slap do
+if game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character.Humanoid.Health ~= 0 then
+game:GetService("ReplicatedStorage").b:FireServer(v:WaitForChild("Head"))
+end
+task.wait()
+end
+end
+                 end
+end
+wait(0.05)
+end
+wait(3)
+if _G.Setting.ChooseWillEnd == "Reset" then
+if game.Players.LocalPlayer.Character.Humanoid.Health ~= 0 then
+game:GetService("ReplicatedStorage"):WaitForChild("HumanoidDied"):FireServer(game.Players.LocalPlayer.Character,false)
+end
+wait(3.75)
+fireclickdetector(workspace.Lobby.Reverse.ClickDetector)
+elseif _G.Setting.ChooseWillEnd == "Server Hop" then
+local serverList = {}
+for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+		serverList[#serverList + 1] = v.id
+	end
+end
+if #serverList > 0 then
+	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)])
+end
+elseif _G.Setting.ChooseWillEnd == "Reset + Loop" then
+if game.Players.LocalPlayer.Character.Humanoid.Health ~= 0 then
+game:GetService("ReplicatedStorage"):WaitForChild("HumanoidDied"):FireServer(game.Players.LocalPlayer.Character,false)
+end
+wait(3.75)
+fireclickdetector(workspace.Lobby.Reverse.ClickDetector)
+if _G.Setting.ResetLoop == true then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Playgiang/Slap_Battles/main/File/Farm_Slap_Ramke.lua"))()
+end
+end
+end
+end
