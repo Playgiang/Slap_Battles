32dff61b635fcae12399e1f0463af64963258acc
AmongUs <128269328+tanhoangviet@users.noreply.github.com>
2024-10-02 20:53:14 +0700

    Update Example.lua

--- LinoriaLib/Example.lua
+++ LinoriaLib/Example.lua
@@ -1,9 +1,9 @@
 -- New example script written by wally
 -- You can suggest changes with a pull request or something
 
-local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+local repo = 'https://raw.githubusercontent.com/Playgiang/Slap_Battles/refs/heads/main/LinoriaLib/'
 
-local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local Library = loadstring(game:HttpGet(repo .. 'test.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
