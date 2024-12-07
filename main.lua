2d565e55b0dd4541dd15df4ac3ec35e117cd4b1f
AmongUs <128269328+tanhoangviet@users.noreply.github.com>
2024-12-07 19:22:12 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
-_G.GameType = "Null"
+_G.GameType = "None"
 
-if
+if game.PlaceId == 6403373529 or game.PlaceId == 9015014224 or game.PlaceId == 11520107397 then
+_G.GameType = "Slap_Battles"
+end
+if game.PlaceId == 4872321990 then 
+_G.GameType = "Islands"
+end
+if game.PlaceId == 9772878203 then 
+_G.GameType = "Raise-A-Floppy-2"
+end
+if _G.GameType = "None" then
+Print("Invaild Game")
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Playgiang/Slap_Battles/refs/heads/main/scr/" .. _G.GameType .. ".lua"))()
